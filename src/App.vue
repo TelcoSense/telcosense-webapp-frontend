2fa455cacd06ac412f7cbc3f731e0f585f2c26c2
stepanmk <smiklanek@gmail.com>
2025-11-10 19:34:58 +0100

    try fixing hover on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+
+if ('ontouchstart' in window || navigator.maxTouchPoints > 0) {
+  document.body.classList.add('no-hover')
+}
 </script>
 
 <template>
