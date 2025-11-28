758670ff65c04cb6c9ae60c978f1322a3193ba85
stepanmk <smiklanek@gmail.com>
2025-11-28 16:43:54 +0100

    try update raincz coords

--- src/stores/layers.ts
+++ src/stores/layers.ts
@@ -18,10 +18,17 @@ export const useLayersStore = defineStore('layers', () => {
     }),
   )
 
+  // const raincz = shallowRef(
+  //   useImageLayer('raincz', {
+  //     apiUrl: '/raincz/list',
+  //     bounds: L.latLngBounds([48.5525, 12.0905], [51.0557, 18.8591]),
+  //   }),
+  // )
+
   const raincz = shallowRef(
     useImageLayer('raincz', {
       apiUrl: '/raincz/list',
-      bounds: L.latLngBounds([48.5525, 12.0905], [51.0557, 18.8591]),
+      bounds: L.latLngBounds([48.55547303756637, 12.094991576420597], [51.05353169070471, 18.8503225129994]),
     }),
   )
 
